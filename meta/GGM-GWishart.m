ef5c7dde6c83e4d5d0525dd616f82fe2c35c90c7
mattbdunham <mattbdunham@5beb9732-0aae-11df-b905-2f917c5cb467>
2010-06-25 17:14:08 +0000

    git-svn-id: https://pmtksupport.googlecode.com/svn/trunk@34 5beb9732-0aae-11df-b905-2f917c5cb467

--- meta/GGM-GWishart.m
+++ meta/GGM-GWishart.m
@@ -1,7 +1,4 @@
-%PMTKtitle GGM-GWishart: Marginal likelihood for non-decomposable Gaussian
-%graphical models using G-Wishart prior (MC, Laplace and BIC
-%approximations)
-%PMTKauthor Ben Marlin, Kevin Murphy, Mark Schmidt, Baback Moghaddam,
-% Beatrix Jones, Carlos Carvalho, Adrian Dobra, Chris Hans, Chris Carter, Mike West  
+%PMTKtitle GGM-GWishart: Marginal likelihood for non-decomposable Gaussian graphical models using G-Wishart prior (MC, Laplace and BIC approximations)
+%PMTKauthor Ben Marlin, Kevin Murphy, Mark Schmidt, Baback Moghaddam, Beatrix Jones, Carlos Carvalho, Adrian Dobra, Chris Hans, Chris Carter, Mike West  
 %PMTKurl NA
 %PMTKdate 24 June 2010
