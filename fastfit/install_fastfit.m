e57337219c18e494c7978c280de5b122d42af5c6
mattbdunham <mattbdunham@5beb9732-0aae-11df-b905-2f917c5cb467>
2010-08-11 22:03:48 +0000

    git-svn-id: https://pmtksupport.googlecode.com/svn/trunk@75 5beb9732-0aae-11df-b905-2f917c5cb467

--- fastfit/install_fastfit.m
+++ fastfit/install_fastfit.m
@@ -1,4 +1,5 @@
-mex pochhammer.c ../lightspeed/util.obj -I../lightspeed
-mex di_pochhammer.c ../lightspeed/util.obj -I../lightspeed
+cd(fileparts(which(mfilename))); 
+mex pochhammer.c ../lightspeed2.3/util.obj -I../lightspeed2.3
+mex di_pochhammer.c ../lightspeed2.3/util.obj -I../lightspeed2.3
 %mex tri_pochhammer.c ../lightspeed/util.obj -I../lightspeed
 %mex s_derivatives.c ../lightspeed/util.obj -I../lightspeed
