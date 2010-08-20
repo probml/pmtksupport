b55898768fb5bfad91f00ae2aba7576de8842cbe
mattbdunham <mattbdunham@5beb9732-0aae-11df-b905-2f917c5cb467>
2010-08-20 17:23:01 +0000

    git-svn-id: https://pmtksupport.googlecode.com/svn/trunk@104 5beb9732-0aae-11df-b905-2f917c5cb467

---
+++ meta/libdai-0.2.6-meta.m
@@ -0,0 +1,4 @@
+% PMTKurl http://people.kyb.tuebingen.mpg.de/jorism/libDAI/
+% PMTKauthor Joris M. Mooij et al
+% PMTKtitle libDAI: C++ library for Discrete Approximate Inference in graphical models
+% PMTKdate August 5, 2010
--- pmtkSupportRoot.m
+++ pmtkSupportRoot.m
@@ -1,4 +1,7 @@
 function r = pmtkSupportRoot()
 % Return directory name where pmtkSupport is stored
-r = fileparts(which(mfilename()));
+w = which(mfilename());
+if w(1) == '.'
+    w = fullfile(pwd, w(3:end));
 end
+r = fileparts(w);
