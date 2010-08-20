12709308cf0a03171a40399d61c43d38864300a6
mattbdunham <mattbdunham@5beb9732-0aae-11df-b905-2f917c5cb467>
2010-08-20 19:11:13 +0000

    git-svn-id: https://pmtksupport.googlecode.com/svn/trunk@109 5beb9732-0aae-11df-b905-2f917c5cb467

--- GGM-GWishart/utility/cov2cor.m
+++
@@ -1,8 +0,0 @@
-function [R,S] = cov2cor(V)
-% Compute the correlation matrix from the covariance matrix
-% Sigma(i) = sqrt( Covariance(i,i) );
-% Corr(i,j) = Covariance(i,j)/( Sigma(i)*Sigma(j) );
-
-S = sqrt(diag(V));
-R = V ./ (S*S');
-
