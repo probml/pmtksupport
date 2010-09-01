f634fd07bd0ecaea2b3ac739a4928dc090c31745
murphyk2 <murphyk2@5beb9732-0aae-11df-b905-2f917c5cb467>
2010-09-01 17:06:05 +0000

    git-svn-id: https://pmtksupport.googlecode.com/svn/trunk@161 5beb9732-0aae-11df-b905-2f917c5cb467

--- meta/markSchmidt-21june2010-meta.m
+++ meta/markSchmidt-21june2010-meta.m
@@ -1,4 +1,4 @@
 %PMTKauthor Mark Schmidt
-%PMTKurl http://www.cs.ubc.ca/~schmidtm/
-%PMTKdate June 7, 2010
+%PMTKurl http://www.cs.ubc.ca/~schmidtm/Software/code.html
+%PMTKdate 21 June  2010
 %PMTKtitle Various optimization and machine learning packages
