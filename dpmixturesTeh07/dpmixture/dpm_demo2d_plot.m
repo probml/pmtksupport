3e2ae4ff51a6e1a81962f85163f5e2fbd062e315
murphyk2 <murphyk2@5beb9732-0aae-11df-b905-2f917c5cb467>
2010-11-23 02:11:09 +0000

    git-svn-id: https://pmtksupport.googlecode.com/svn/trunk@205 5beb9732-0aae-11df-b905-2f917c5cb467

--- dpmixturesTeh07/dpmixture/dpm_demo2d_plot.m
+++ dpmixturesTeh07/dpmixture/dpm_demo2d_plot.m
@@ -5,11 +5,11 @@ ih = ishold;
 
 for kk = 1:dpm.KK
   [mu sigma] = rand(dpm.qq{kk});
-  plotellipse(mu,sigma,'color',cc(kk,:));
+  plotellipse(mu,sigma,'color',cc(kk,:),'linewidth',2);
   hold on
   ii = find(dpm.zz==kk);
   xx = cat(2,dpm.xx{ii});
-  plot(xx(1,:),xx(2,:),'.','color',cc(kk,:));
+  plot(xx(1,:),xx(2,:),'.','color',cc(kk,:),'markersize',10);
 end
 
 if ~ih, hold off; end
